2685fcee1e1fc4fb65bae19b4fe389e37b9ef204
charoufnassro <charoufnassro@gmail.com>
2019-09-26 13:17:48 +0100

    Event Booking first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphQlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+const app = express();
+
+const events = [];
+
+app.use(bodyParser.json());
+app.use('/graphql', graphQlHttp({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootQuery{
+            events:[Event!]!
+        }
+
+        type RootMutation{
+            createEvent(eventInput: EventInput): Event
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }    
+    `),
+    rootValue: {
+        events: ()=>{
+            return events;
+        },
+        createEvent: (args) => {
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date,
+            };
+            events.push(event);
+            return event
+        }
+    },
+    graphiql: true
+}))
+
+app.listen(3000, ()=>{
+    console.log('server start at http://localhost:3000')
+});
