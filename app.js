0a4da164d8c2527230be5da8c495d6ee29a722cf
charoufnassro <charoufnassro@gmail.com>
2019-09-29 15:02:42 +0100

    organize code

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ app.use('/graphql', graphQlHttp({
     rootValue: graphQLResolvers,
     graphiql: true
 }))
-// mongodb://localhost:27017/eventbooking
+// mongodb+srv://${process.env.MONGO_USER}:STBuIT6yLMM03C6v@usermanager-cg1jd.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority
 console.log(process.env.MONGO_USER)
 console.log(process.env.MONGO_PASSWORD)
 console.log(process.env.MONGO_DB)
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:STBuIT6yLMM03C6v@usermanager-cg1jd.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }).then(
+mongoose.connect(`mongodb://localhost:27017/eventbooking`, { useNewUrlParser: true, useUnifiedTopology: true }).then(
     ()=> {
         app.listen(3000, ()=> {
             console.log('server start at http://localhost:3000 ')
--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -3,37 +3,73 @@ const Event = require('../../models/event');
 const User = require('../../models/user');
 
 
+const events = async eventIds => {
+    try{
+        const events = await Event.find({_id: {$in: eventIds}});
+        return events.map(
+                    event => {
+                        return{ 
+                            ...event._doc,
+                            _id: event.id,
+                            creator: user.bind(this, event.creator)
+                        }
+                    }
+                )
+    }catch(err){
+        throw err
+    }
+    
+}
+
+const user = async userId => {
+    try{
+        const user = await User.findById(userId)
+        return { 
+            ...user._doc,
+            _id: user.id,
+            password: null,
+            createdEvents: events.bind(this, user._doc.createEvents)
+        };
+    }catch(err){
+        throw err
+    }
+}
+
 module.exports = {
-                    events: ()=>{
-                        return Event.find()
-                            .populate('creator')
-                                .then(events => {
-                                    return events.map(event =>{
-                                        return { 
-                                            ...event._doc,
-                                            _id: event.id,
-                                            date: new Date(event.date).toISOString()
-                                        };
-                                    });
-                                })
-                                    .catch(err => {
-                                        throw err;
-                                    });
+                    events: async () => {
+                        try{
+                            const events = await Event.find();
+                            return events.map(event =>{
+                                return { 
+                                    ...event._doc,
+                                    _id: event.id,
+                                    date: new Date(event.date).toISOString(),
+                                    creator: user.bind(this, event._doc.creator)
+                                };
+                            });
+                        }catch(err){
+                            throw err
+                        }
                     },
                     createEvent: args => {
                         const event = new Event({
                             title: args.eventInput.title,
                             description: args.eventInput.description,
                             price: +args.eventInput.price,
-                            date: new Date(args.eventInput.date),
-                            creator: '5d8f249d50d3561eac9ed8d6'
+                            date: new Date(args.eventInput.date).toISOString(),
+                            creator: '5d9076066847e1265ced7a70'
                         });
                         let createdEvent;
                         return event.save()
                                 .then( resultEevent => {
                                     console.log(resultEevent)
-                                    createdEvent = { ...resultEevent._doc, _id: resultEevent.id.toString()}
-                                    return User.findById('5d8f249d50d3561eac9ed8d6');
+                                    createdEvent = { 
+                                        ...resultEevent._doc,
+                                        _id: resultEevent.id.toString(),
+                                        date: new Date(args.eventInput.date).toISOString(),
+                                        creator: user.bind(this, resultEevent.creator) 
+                                    }
+                                    return User.findById('5d9076066847e1265ced7a70');
                                 })
                                     .then( user => {
                                         if(!user) {
